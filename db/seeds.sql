5b814d84afefb2d129a8741b3791ec66ce9fcf53
Lexi Geller <leximoconner@yahoo.com>
2023-07-11 16:24:41 -0400

    fixed error in seeds.sql file

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,7 +10,7 @@ INSERT INTO role (title, salary, department_id) VALUES
   ('Regional Manager', 60000.00, 1),
   ('Assistant to the Regional Manager', 40000.00, 1),
   ('Sales Representative', 40000.00, 2),
-  ('Packer', 3000.00, 3),
+  ('Packer', 30000.00, 3),
   ('HR Representative', 38000.00, 4),
   ('HR Manager', 51000.00, 4),
   ('Receptionist', 20000.00, 5),
@@ -19,7 +19,7 @@ INSERT INTO role (title, salary, department_id) VALUES
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
   ('Michael', 'Scott', 1, NULL),
   ('Dwight', 'Schrute', 2, 1),
-  ('Jim', 'Halpert', 3, 1),
+  ('Jim', 'Halpert', 3, 2),
   ('Pam', 'Beesley', 4, 3),
   ('Darryl', 'Philbin', 5, NULL),
   ('Phyllis', 'Vance', 6, 5),
