fe820bfbc47cd6bc7f482b4222dd2ec86c580451
Lexi Geller <leximoconner@yahoo.com>
2023-07-10 20:58:31 -0400

    added drop/create database

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,8 @@
+DROP DATABASE IF EXISTS employee_tracker;
+CREATE DATABASE employee_tracker;
+
+USE employee_tracker;
+
 CREATE TABLE department (
   id INT PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(30) NOT NULL
